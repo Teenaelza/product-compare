29ec175dc8395abefbc6fc7d0ec5c565c2f087b6
Teena <87797429+Teenaelza@users.noreply.github.com>
2021-11-08 22:27:14 -0800

    Initialize feature/save-comparison-db

---
+++ client/src/components/Product.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './Product.css';
+
+
+
+export default function Product(props) {
+  
+  
+  const [selected, setSelected] = useState(false);
+
+  const toggleSelected = () => {
+    if (selected) {
+      setSelected(false);
+      props.removeProdIDs(props.id);
+      props.handleSelect(false, props.id, props)
+      
+      
+    } else {
+      setSelected(true);
+      props.addProdIDs(props.id);
+      props.handleSelect(true, props.id, props);
+    }
+  };
+
+  return (
+    <div className="col" >
+      
+      <div className="card">
+        <div className='card-body'>
+          <div>
+            {!selected && <i onClick={() => {toggleSelected()}} className="fas fa-compress" id={props.id} name={props.name}></i>}
+            {selected && <i onClick={() => {toggleSelected()}} className="fas fa-compress-arrows-alt" id={props.id} name={props.name}></i>}
+          </div>
+          <img src={props.image} className="first-image" alt="" />
+          <hr />
+          <center>
+            <h5 className="card-title">{props.name}</h5>
+          </center>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
