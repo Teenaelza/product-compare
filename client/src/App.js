0bb2d592e6adb763657f52ad3a61be24e5c7a5d3
Teena <87797429+Teenaelza@users.noreply.github.com>
2021-11-08 22:27:12 -0800

    Initialize products/features

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: 'Click the button to load data!'
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{ this.state.message }</h1>
+        <button onClick={this.fetchData} >
+          Fetch Data
+        </button>        
+      </div>
+    );
+  }
+}
+
+export default App;
